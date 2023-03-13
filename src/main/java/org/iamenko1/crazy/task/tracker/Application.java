fd38eafbf1416edf00d5e2f6ebd350455b2ede2c
MenkovIvan <menkov_99@mail.ru>
2023-03-14 01:05:54 +0300

    add Application.java

---
+++ src/main/java/org/iamenko1/crazy/task/tracker/Application.java
@@ -0,0 +1,13 @@
+package org.iamenko1.crazy.task.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication
+                .run(Application.class, args);
+    }
+}
