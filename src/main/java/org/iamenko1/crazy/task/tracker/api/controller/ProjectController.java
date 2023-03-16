2692cd00f4a2b1f51b1883afe1fbb1063b16308a
MenkovIvan <menkov_99@mail.ru>
2023-03-16 17:59:10 +0300

    add method createProject

--- src/main/java/org/iamenko1/crazy/task/tracker/api/controller/ProjectController.java
+++ src/main/java/org/iamenko1/crazy/task/tracker/api/controller/ProjectController.java
@@ -1,8 +1,13 @@
 package org.iamenko1.crazy.task.tracker.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.iamenko1.crazy.task.tracker.api.dto.ProjectDto;
+import org.iamenko1.crazy.task.tracker.api.exception.BadRequestException;
 import org.iamenko1.crazy.task.tracker.factory.ProjectDtoFactory;
+import org.iamenko1.crazy.task.tracker.store.entity.ProjectEntity;
 import org.iamenko1.crazy.task.tracker.store.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
@@ -15,4 +20,23 @@ public class ProjectController {
     private final ProjectRepository projectRepository;
     private final ProjectDtoFactory projectDtoFactory;
 
+    public static final String CREATE_PROJECT = "/api/projects";
+
+    @PostMapping(CREATE_PROJECT)
+    public ProjectDto createProject(@RequestBody ProjectDto projectDto){
+
+        projectRepository
+                .findByName(projectDto.getName())
+                .ifPresent(projectEntity -> {
+                    throw new BadRequestException(String.format("Project \"%s\" already exists.", projectDto.getName()));
+                });
+
+        ProjectEntity projectEntity = projectRepository.saveAndFlush(
+                ProjectEntity.builder()
+                        .name(projectDto.getName())
+                        .build()
+        );
+
+        return projectDtoFactory.makeProjectDto(projectEntity);
+    }
 }
--- src/main/java/org/iamenko1/crazy/task/tracker/store/repository/ProjectRepository.java
+++ src/main/java/org/iamenko1/crazy/task/tracker/store/repository/ProjectRepository.java
@@ -3,5 +3,9 @@ package org.iamenko1.crazy.task.tracker.store.repository;
 import org.iamenko1.crazy.task.tracker.store.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+
+    Optional<ProjectEntity> findByName(String name);
 }
