a8a364a45ad9264d07f5be65b321da4565f484b6
MenkovIvan <menkov_99@mail.ru>
2023-03-17 18:17:17 +0300

    add methods with Stream to ProjectRepository

--- src/main/java/org/iamenko1/crazy/task/tracker/store/repository/ProjectRepository.java
+++ src/main/java/org/iamenko1/crazy/task/tracker/store/repository/ProjectRepository.java
@@ -4,8 +4,13 @@ import org.iamenko1.crazy.task.tracker.store.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 
     Optional<ProjectEntity> findByName(String name);
+
+    Stream<ProjectEntity> streamAllBy();
+
+    Stream<ProjectEntity> streamAllByNameStartsWithIgnoreCase(String prefix);
 }
